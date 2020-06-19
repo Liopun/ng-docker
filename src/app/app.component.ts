5ca6af24a877aabec4958ced3a971c5efc2f9208
Captone Habiyaremye <hcaptone@gmail.com>
2020-06-19 14:19:28 -0400

    added dockerfile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-docker';
+  title = 'Dockerizing An AngularCLI App';
 }
